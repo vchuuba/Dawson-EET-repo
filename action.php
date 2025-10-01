c2b9d7321d0bd2a35f693f721a0201ea33a8a408
Richardel Leang <richardel.leang@dawsoncollege.qc.ca>
2025-10-01 10:10:24 -0400

    Update action.php

--- action.php
+++ action.php
@@ -5,6 +5,9 @@
     </head>
     <body>
         <p><?= var_dump($_SERVER) ?></p>
+	<p>GET: <?= var_dump($_GET) ?></p> 
+	<p>POST: <?= var_dump($_POST) ?></p>
+
         <p>Your name is: <?= htmlspecialchars($_POST['fname']) . " " . htmlspecialchars($_POST['lname'])?></p>
         <p>Your favorite language is <?= $_POST(['fav_language'])?></p>
     </body>
