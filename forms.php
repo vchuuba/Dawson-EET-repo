3b2d48a02ada4528091bd2fc2197d35b8658ed96
Richardel Leang <richardel.leang@dawsoncollege.qc.ca>
2025-10-01 14:13:06 -0400

    second php post test

--- forms.php
+++ forms.php
@@ -2,10 +2,10 @@
 <html>
     <head>
         <title>Form response</title>
-    </head>
-    <body>
         <?= var_dump($_GET) ?> 
         <?= var_dump($_POST) ?>
+    </head>
+    <body>
         <p>Hello, your name is <?= $_POST['fname'] ?> <?= $_POST['lname'] ?>. Your favorite language is <?= $_POST['fav_language'] ?>.</p>
     </body>
 </html>
