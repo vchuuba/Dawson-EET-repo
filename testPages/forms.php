ed8f1908fd064f40e7c441af78703a9764a7c038
Richardel Leang <richardel.leang@dawsoncollege.qc.ca>
2025-10-15 13:40:17 -0400

    is it the php equal sign

--- testPages/forms.php
+++ testPages/forms.php
@@ -8,7 +8,7 @@
         <p><?= var_dump($_POST) ?></p>
         <p>Hello, your name is <?= $_POST['fname'] ?> <?= $_POST['lname'] ?>. Your favorite language is <?= $_POST['fav_language'] ?>.</p>
 
-        <?php
+        <?php=
         $servername = "thelichking"; // keep $servername to localhost if doing it on own Pi
         $username = "Arthas";
         $password = "Shell111";
