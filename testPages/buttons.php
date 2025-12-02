f127e8fc86d87d22646b3aaaea7a59021bcf0a3c
Richardel Leang <richardel.leang@dawsoncollege.qc.ca>
2025-12-02 15:43:38 -0500

    GPIO ATTEMPT TEST

--- testPages/buttons.php
+++ testPages/buttons.php
@@ -7,27 +7,43 @@
         </script> -->
     </head>
     <body>
-        <?php=
-        $servername = "thelichking"; // keep $servername to localhost if doing it on own Pi
-        $username = "Arthas";
-        $password = "Shell111";
-        $dbname = "unitStats";
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check for successful connection
-        if (!$conn) { 
-            die("Connection failed: {mysqli_connect_error()}"); 
-        } 
-        echo "Connected successfully";
-        
-        $toggle = `gpio toggle 0`;
-        $read = `gpio read 0`;
-        echo $read;
-        echo "test";
-                
-        mysqli_close($conn);
+      <p>
+        <?php
+          $servername = "localhost"; // keep $servername to localhost if doing it on own Pi
+          $username = "Arthas";
+          $password = "Shell111";
+          $dbname = "unitStats";
+          // Create connection
+          $conn = mysqli_connect($servername, $username, $password, $dbname);
+          // Check for successful connection
+          if (!$conn) { 
+              die("Connection failed: {mysqli_connect_error()}"); 
+          } 
+          echo "Connected successfully";
 
+          // $sql = "select * from unitList where id={$_POST['unitID']};";		// This can be any valid SQL cmd.
+          $sql = "select * from unitList where unitFaction like \"%{$_POST['unitFaction']}%\";";		// This can be any valid SQL cmd.
+          $result = mysqli_query($conn, $sql);	// Stores rows retrieved by query.
+          echo mysqli_error($conn);	    		// If error, this determines cause.
         ?>
+      </p>
+
+      <p>
+        <?php
+          // foreach($result as $row) { 
+          // echo nl2br ("\n id: {$row["id"]}
+          // | Unit: {$row["unitName"]} 
+          // | Faction: {$row["unitFaction"]}
+          // | Class: {$row["unitClass"]}\n"); }
+          // // This iterates through every each row and echoes only values of defined columns.
+          $toggle = `gpio toggle 0`;
+          $read = `gpio read 0`;
+          echo $read;
+          echo "test";
+          
+          mysqli_close($conn);
+        ?>
+      </p>
 
         <a href="buttons.html">Go back.</a>
 
