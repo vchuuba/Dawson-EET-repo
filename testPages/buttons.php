f674fe57604080bb2d24aedda0ca867b0ef9f8c3
Richardel Leang <richardel.leang@dawsoncollege.qc.ca>
2025-12-02 15:21:33 -0500

    attempt gpio toggle

---
+++ testPages/buttons.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GPIO toggle</title>
+        <script>
+          window.location.replace("buttons.html");
+        </script>
+    </head>
+    <body>
+        <?php=
+        $servername = "thelichking"; // keep $servername to localhost if doing it on own Pi
+        $username = "Arthas";
+        $password = "Shell111";
+        $dbname = "unitStats";
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check for successful connection
+        if (!$conn) { 
+            die("Connection failed: {mysqli_connect_error()}"); 
+        } 
+        echo "Connected successfully";
+        
+        `gpio toggle 0`;
+                
+        mysqli_close($conn);
+
+        ?>
+
+
+    </body>
+</html>
